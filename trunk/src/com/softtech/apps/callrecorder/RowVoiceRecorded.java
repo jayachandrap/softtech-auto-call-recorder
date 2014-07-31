1eeb9f636def4d93e259b0ab16e235db2996d240
loicuoi@gmail.com <loicuoi@gmail.com@8868282d-fc70-df1b-3ccd-d0eff21bfe72>
2014-07-31 10:42:54 +0000

    - Sua lai duong dan khi ghi file

--- trunk/src/com/softtech/apps/callrecorder/RowVoiceRecorded.java
+++ trunk/src/com/softtech/apps/callrecorder/RowVoiceRecorded.java
@@ -6,15 +6,26 @@ public class RowVoiceRecorded {
 	private String mPath;
 	private String mPhoneNumber;
 	private Long mTimeCreate;
+	private boolean isSync = false;
+
 	public RowVoiceRecorded() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public RowVoiceRecorded(String Name, String Path, Long TimeCreate,String phoneNumber) {
+	public RowVoiceRecorded(String Name, String Path, Long TimeCreate,String phoneNumber, boolean is_sync) {
 		this.mName = Name;
 		this.mPath = Path;
 		this.mTimeCreate = TimeCreate;
 		this.mPhoneNumber = phoneNumber;
+		this.isSync = is_sync;
+	}
+	
+	public boolean isSync() {
+		return isSync;
+	}
+
+	public void setSync(boolean isSync) {
+		this.isSync = isSync;
 	}
 
 	public String getmName() {
